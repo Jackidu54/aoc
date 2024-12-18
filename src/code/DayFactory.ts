8da28ee34825a5188046872b08c48f97972fd8b2
ajacque <a.jacque.5498@gmail.com>
2024-12-18 14:37:36 +0100

    D7

--- src/code/DayFactory.ts
+++ src/code/DayFactory.ts
@@ -5,6 +5,7 @@ import {D03} from "./Days/2024/D03.ts";
 import {D04} from "./Days/2024/D04.ts";
 import {D05} from "./Days/2024/D05.ts";
 import {D06} from "./Days/2024/D06.ts";
+import {D07} from "./Days/2024/D07.ts";
 class DayFactory {
     create(date: Date): DaySolver {
         const dateStr = date.getFullYear() + "" + (date.getDay()+1)
@@ -15,6 +16,7 @@ class DayFactory {
             case "20244": return new D04(date);
             case "20245": return new D05(date);
             case "20246": return new D06(date);
+            case "20247": return new D07(date);
             default: return new class extends DaySolver {
                 compute2(input: string): string {
                     return input;
---
+++ src/code/Days/2024/D07.ts
@@ -0,0 +1,58 @@
+import {DaySolver} from "../../DaySolver.ts";
+
+export class D07 extends DaySolver {
+    compute1(input: string): string {
+        return "" + input.split('\n')
+            .map(l => ({result: parseInt(l.split(": ")[0]), vals: l.split(": ")[1].split(" ").map(i => parseInt(i))}))
+            .filter(l => this.generateOperations(l.vals, ['+', '*']).some(op => op.result === l.result))
+            .reduce((acc, curr) => acc + curr.result, 0)
+
+    }
+
+
+    compute2(input: string): string {
+        return "" + input.split('\n')
+            .map(l => ({result: parseInt(l.split(": ")[0]), vals: l.split(": ")[1].split(" ").map(i => parseInt(i))}))
+            .filter(l => this.generateOperations(l.vals, ['+', '*', 'c']).some(op => op.result === l.result))
+            .reduce((acc, curr) => acc + curr.result, 0)
+    }
+
+    generateOperations(numbers: number[], operators: string[]): { operation: string, result: number }[] {
+        const results: { operation: string, result: number }[] = [];
+
+        function evaluateLeftToRight(expression: string): number {
+            const tokens = expression.split(/(\+|\*|c)/);
+            let result = parseInt(tokens[0]);
+            for (let i = 1; i < tokens.length; i += 2) {
+                const operator = tokens[i];
+                const nextNumber = parseInt(tokens[i + 1]);
+                if (operator === '+') {
+                    result += nextNumber;
+                } else if (operator === '*') {
+                    result *= nextNumber;
+                } else if (operator === 'c') {
+                    result = parseInt(result + '' + nextNumber);
+                }
+            }
+            return result;
+        }
+
+        function helper(current: string, index: number) {
+            if (index === numbers.length) {
+                results.push({operation: current, result: evaluateLeftToRight(current)});
+                return;
+            }
+
+            for (const operator of operators) {
+                helper(current + operator + numbers[index], index + 1);
+            }
+        }
+
+        if (numbers.length > 0) {
+            helper(numbers[0].toString(), 1);
+        }
+
+        return results;
+    }
+
+}
