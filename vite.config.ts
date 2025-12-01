0de74c5d4172aa8f884df79449e424a0e4029460
ajacque <a.jacque.5498@gmail.com>
2025-12-01 16:03:47 +0100

    remove config and session ID

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         proxy: {
             '/input': {
                 secure: false,
-                headers: {'cookie': 'session=53616c7465645f5fcb3b1b421b46b40d1a8ef710e3314651c2bf5dff1749966121455e8d2f870903e3433642e1a3c38d8154648c7d56b3631720074def330ad6'},
+                headers: {'cookie': 'session='},
                 target: 'https://adventofcode.com',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/input/, ''),
